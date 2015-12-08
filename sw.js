7eb207fb0e10dc0da69d9bf474a9d76ece157be0
Tim Volodine <timvolodine@google.com>
2015-12-08 15:44:28 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,14 +2,18 @@ console.log("fetch", this.fetch);
 
   fetch('https://timvolodine.github.io/service-worker-test/sec-breach.html').then(function(res) {
   console.log("Response", res);
-  return res.text();
+  
+  sometext = res.text();
+  
+//  return res.text();
 
-/*
+
 self.addEventListener('fetch', function(event) {
   console.log("Caught a fetch!");
-  event.respondWith(new Response("ALOHA!"));
+  //event.respondWith(new Response("ALOHA!"));
+  event.respondWith(new Response(sometext));
 }
-*/
+
 
 });
 
