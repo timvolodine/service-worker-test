74e6262a64449df6b08d28d1766bb8e9e8bc629c
Tim Volodine <timvolodine@google.com>
2015-12-08 13:29:51 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
 console.log("fetch", this.fetch);
 
+  fetch('./').then(function(res) {
+  console.log("Response", res);
+  return res.text();
+
+/*
 self.addEventListener('fetch', function(event) {
   console.log("Caught a fetch!");
   event.respondWith(new Response("ALOHA!"));
+}
+*/
+
 });
 
-fetch('./').then(function(res) {
-  console.log("Response", res);
-  return res.text();
-}
