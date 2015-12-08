fda478242dea2edbc22dfb23e3d7e4324623d582
Tim Volodine <timvolodine@google.com>
2015-12-08 19:21:07 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,31 +6,22 @@ console.log("fetch", this.fetch);
     console.log("Response", res);
     res.blob().then(function(res2) {
        sometext = res2;
-       
+    });
+  });
+//});
+
+
+
+self.addEventListener('fetch', function(event) {
+  console.log("Caught a fetch!");
+
+  //event.respondWith(new Response("ALOHA!"));
+//  event.respondWith(new Response(sometext));
        clients.matchAll().then(function(clients) {
          console.log("clients.length=",clients.length);
          for (i=0;i<clients.length;i++){
            clients[i].postMessage(sometext);
          }
        });
-    });
-  });
-//});
-
-
-/*
-self.addEventListener('fetch', function(event) {
-  console.log("Caught a fetch!");
-//  fetch('https://timvolodine.github.io/service-worker-test/sec-breach.html').then(function(res) {
-//    console.log("Response", res);
-//    event.respondWith(new Response(res.text()));
-//  });
-//  sometext = res.text();
-
-  //event.respondWith(new Response("ALOHA!"));
-  event.respondWith(new Response(sometext));
-//  event.respondWith(new Response(res.text()));
-
-
 });
-*/
+
