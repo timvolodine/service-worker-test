379254ea5fea4ddb26931946aa6322b0bd3892dd
Tim Volodine <timvolodine@google.com>
2015-12-08 13:28:57 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,8 @@ self.addEventListener('fetch', function(event) {
   console.log("Caught a fetch!");
   event.respondWith(new Response("ALOHA!"));
 });
+
+fetch('./').then(function(res) {
+  console.log("Response", res);
+  return res.text();
+}
