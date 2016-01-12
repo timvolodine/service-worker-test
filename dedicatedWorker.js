ccd7e806b1b91c44547363686267f77ad390f7c4
Tim Volodine <timvolodine@google.com>
2016-01-12 13:45:03 +0000

    Update dedicatedWorker.js

--- dedicatedWorker.js
+++ dedicatedWorker.js
@@ -1,1 +1,9 @@
 //
+fetch('https://timvolodine.github.io/service-worker-test/sec-breach-dedicated.html').then(function(res) {
+    console.log("Response", res);
+    res.text().then(function(res2) {
+       sometext = res2;
+       console.log(sometext);
+    });
+  });
+  
